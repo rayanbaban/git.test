434522958a28ea578c106df79eaa8c480b41d5b0
Rayan Baban <Rayan.baban@gokstadakademiet.no>
2023-01-30 19:24:56 +0100

    lagt til en attribut

---
+++ qiii/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace qiii
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            artistInfo  NY = new artistInfo("50 Cent", "Get rich or die tryin", 2003, 872.000f);
+            artistInfo  CA = new artistInfo("The Game", "The Documentary", 2005, 586.000f);
+
+            Console.WriteLine(NY.artistNavn + " Sitt første album heter " + NY.albumNavn + " ble utgitt i år: " 
+                + NY.utgivelseÅr + " og solgte: " + NY.albumSolgt + " den første uken");
+            
+        }
+    }
+}
---
+++ qiii/artistinfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qiii
+{
+    class artistInfo
+    {
+        public string artistNavn;
+        public string albumNavn;
+        public int utgivelseÅr;
+        public float albumSolgt;
+    
+        public artistInfo (string argArtistNavn, string argAlbumNavn, int argUtgivelsesÅr, float argAlbumSolgt)
+        {
+            artistNavn = argArtistNavn;
+            albumNavn = argAlbumNavn;
+            utgivelseÅr = argUtgivelsesÅr;
+            albumSolgt = argAlbumSolgt;
+
+        }
+    }
+}
